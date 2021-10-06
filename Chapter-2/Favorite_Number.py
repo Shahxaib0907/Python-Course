96513d4ea79bb6af182a2d690e37fd0647586363
Shahxaib0907 <63181085+Shahxaib0907@users.noreply.github.com>
2021-10-06 10:06:24 +0500

    Ex. 2.9

---
+++ Chapter-2/Favorite_Number.py
@@ -0,0 +1,9 @@
+# Ex2.9 Favorite Number::
+ 
+''' Use a variable to represent your favorite number. Then, 
+using that variable, create a message that reveals your favorite number. Print 
+that message.'''
+
+
+fav_num = "9"
+print("My favourite number is " + fav_num)
