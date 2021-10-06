e04c2538c7ea62579dbe6bf8a1cefce2743d0298
Shahxaib0907 <63181085+Shahxaib0907@users.noreply.github.com>
2021-10-06 10:03:58 +0500

    Ex. 2.4

---
+++ Chapter-2/Name_Cases.py
@@ -0,0 +1,10 @@
+# Ex2.4 Name Cases:
+ 
+''' Use a variable to represent a person’s name, and then print 
+that person’s name in lowercase, uppercase, and title case. '''
+
+
+person_name = "Shahzaib"
+print(person_name.title())
+print(person_name.upper())
+print(person_name.lower())
