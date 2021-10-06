73803bb8a2196b2a4176cf7bc07bf1626f00fdae
Shahxaib0907 <63181085+Shahxaib0907@users.noreply.github.com>
2021-10-06 10:02:46 +0500

    Ex. 2.2

---
+++ Chapter-2/Simple_messages.py
@@ -0,0 +1,12 @@
+# Ex2.2 Simple Messages:
+ 
+''' Assign a message to a variable, and print that message. 
+Then change the value of the variable to a new message, and print the new 
+message. '''
+
+
+my_var = "Hello Guys"
+print(my_var)
+
+my_var = "Python is easy!"
+print(my_var)
