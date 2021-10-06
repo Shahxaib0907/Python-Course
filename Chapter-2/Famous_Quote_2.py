0b4401ae2e8149bfa8386112f42866b22de0aa4f
Shahxaib0907 <63181085+Shahxaib0907@users.noreply.github.com>
2021-10-06 10:04:58 +0500

    Ex. 2.6

---
+++ Chapter-2/Famous_Quote_2.py
@@ -0,0 +1,13 @@
+# Ex2.6 Famous Quote 2:
+ 
+''' Find a quote from a famous person you admire. Print the 
+quote and the name of its author. Your output should look something like the 
+following, including the quotation marks:
+
+Albert Einstein once said, “A person who never made a 
+mistake never tried anything new.” '''
+
+
+person_name = "Albert Einstein"
+message = "“A person who never made a mistake never tried anything new.”"
+print(person_name + 'once said,' + message)
